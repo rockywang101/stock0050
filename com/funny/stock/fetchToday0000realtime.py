b2b73ad91b16be6e8d299b59ca303c2588ac9e3b
rocky.wang <rockywang101@gmail.com>
2017-12-11 18:27:46 +0800

    1

--- com/funny/stock/fetchToday0000realtime.py
+++ com/funny/stock/fetchToday0000realtime.py
@@ -117,7 +117,7 @@ else:
 if msg != None:
     print(msg)
     lineTool.lineNotify(os.environ["LINE_0050_TOKEN"], msg)
-    time.sleep(5)   # delays for n seconds
+    time.sleep(15)   # delays for n seconds
     lineTool.lineNotify(os.environ["LINE_0050_TOKEN2"], msg)
     
     
