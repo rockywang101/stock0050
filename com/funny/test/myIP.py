6a4846c6637ab53f192f45f4d7f0f0acc70176db
Rocky <rockywang101@gmail.com>
2017-12-17 23:25:13 +0800

    add ip detect

---
+++ com/funny/test/myIP.py
@@ -0,0 +1,35 @@
+'''
+Created on 2017年12月17日
+@author: Rocky
+'''
+import requests, bs4, lineTool, os
+import datetime
+
+ip = None
+with open("myip.txt", "r") as f1:
+    ip = f1.read()
+
+url = "http://www.seocheckpoints.com/my-ip-address"
+
+r = requests.get(url)
+soup = bs4.BeautifulSoup(r.text, "html.parser")
+
+elem = soup.find("span", {"class": "badge bg-green"})
+
+todayIP = elem.text
+
+with open("myip.txt", "w") as f1:
+    f1.write(todayIP)
+
+now = datetime.datetime.now()
+print()
+print("執行時間 %s" %(now.strftime("%Y-%m-%d %H:%M:%S")))
+
+# 若不同，發通知
+if ip != todayIP:
+    token = os.environ["LINE_TEST_TOKEN"]
+    msg = "IP change from %s to %s" %(ip, todayIP)
+    lineTool.lineNotify(token, msg)
+    print(msg)
+else:
+    print("ip not change")
