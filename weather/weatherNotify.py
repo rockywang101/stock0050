c9497466cd99abc1427200d660dd6b17091f6f1c
Rocky <rockywang101@gmail.com>
2018-02-21 00:03:48 +0800

    1

--- weather/weatherNotify.py
+++ weather/weatherNotify.py
@@ -10,19 +10,16 @@ import xml.etree.ElementTree as ET
 import requests, os
 import lineTool
 
-dataid = "F-C0032-010"
-url = "http://opendata.cwb.gov.tw/opendataapi?dataid=" + dataid + "&authorizationkey=" + os.environ["OPEN_DATA_CWB_TOKEN"]
+apikey = os.environ["OPEN_DATA_CWB_TOKEN"]
+
+dataid = "F-C0032-029"
+url = "http://opendata.cwb.gov.tw/opendataapi?dataid=" + dataid + "&authorizationkey=" + apikey
 
 xml = ET.fromstring(requests.get(url).text)
 
-msg = xml[8][2][1][0].text + "\n\n" + xml[8][2][2][0].text + "\n\n" + xml[8][2][3][0].text
+msg = xml[8][2][1][0].text + "\n\n" + xml[8][2][2][0].text
+# msg = xml[8][2][1][0].text + "\n\n" + xml[8][2][2][0].text + "\n\n" + xml[8][2][3][0].text
 
-
-print(xml[8][2][1][0].text)
-print()
-print(xml[8][2][2][0].text)
-print()
-print(xml[8][2][3][0].text)
-
-lineTool.lineNotify(os.environ["LINE_TEST_TOKEN"], msg)
+print(msg)
+lineTool.lineNotify(os.environ["LINE_STUPID_TOKEN"], msg)
 
