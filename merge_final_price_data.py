a235e87111730063723de7df0003703f945c9d4f
rocky.wang <rockywang101@gmail.com>
2018-03-02 11:40:29 +0800

    1

--- merge_final_price_data.py
+++ merge_final_price_data.py
@@ -3,35 +3,42 @@ Created on 2018年2月27日
 @author: rocky.wang
 '''
 import os, sys, csv, datetime
+from pathlib import Path
 
 dt = datetime.datetime.now()
 
 #dt = datetime.datetime(2018, 3, 1)
 
 filename = "daily_price/{}.csv".format(dt.strftime("%Y%m%d"))
-
 print(filename)
 
-with open(filename, "r", encoding="utf-8") as f1:
-    csvReader = csv.reader(f1)
-    for row in csvReader:
-        print(row)
-        
-        for i in range(len(row)):
-            row[i] = row[i].replace(",", "")
-        
-        sign = " "
-        if "green" in row[9]:
-            sign = "-"
-        elif "red" in row[9]:
-            sign = "+"
-        sign += row[10]
-        
-        ymd = str(int(dt.strftime("%Y")) - 1911) + "/" + dt.strftime("%m/%d")
-        newRow = [ymd, row[2], row[4], row[5], row[6], row[7], row[8], sign, row[3]]
-        
-        with open("data/{}.csv".format(row[0]), "a", newline="\n", encoding="utf-8") as f2:
-            csvWriter = csv.writer(f2)
-            csvWriter.writerow(newRow)
-        
-        print("end %s" %(row[0]))
+if Path(filename).is_file():
+    
+    print("begin merge")
+    
+    with open(filename, "r", encoding="utf-8") as f1:
+        csvReader = csv.reader(f1)
+        for row in csvReader:
+            print(row)
+            
+            for i in range(len(row)):
+                row[i] = row[i].replace(",", "")
+            
+            sign = " "
+            if "green" in row[9]:
+                sign = "-"
+            elif "red" in row[9]:
+                sign = "+"
+            sign += row[10]
+            
+            ymd = str(int(dt.strftime("%Y")) - 1911) + "/" + dt.strftime("%m/%d")
+            newRow = [ymd, row[2], row[4], row[5], row[6], row[7], row[8], sign, row[3]]
+            
+            with open("data/{}.csv".format(row[0]), "a", newline="\n", encoding="utf-8") as f2:
+                csvWriter = csv.writer(f2)
+                csvWriter.writerow(newRow)
+            
+            print("end %s" %(row[0]))
+            
+else:
+    print("file not exist")
